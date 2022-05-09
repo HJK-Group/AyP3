d26e93d53b8f1a9c7c6d9b10d15187e7d86ce083
Kevin Eloy Tubio <kevtubio@emeal.nttdata.com>
2022-05-09 20:33:00 -0300

    api lista

--- src/com/hjk/tp/list/list.h
+++ src/com/hjk/tp/list/list.h
@@ -1,4 +1,23 @@
 #ifndef AYP3_LIST_H
 #define AYP3_LIST_H
 
+#include <stdio.h>
+
+typedef struct list {
+    struct list *next;
+    void *data;
+} list;
+
+list *new_list(void *data, size_t data_size);
+
+void list_add(list *list, void *data, size_t data_size);
+
+int list_length(list *list);
+
+void *list_get(list* list, int index);
+
+void list_remove(list *list, int index);
+
+void list_print(list *);
+
 #endif //AYP3_LIST_H
