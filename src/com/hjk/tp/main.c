e4d6f5254397b0f8d660b2c33f6c132932e10173
Hernán Rubio <hernan.matias.rubio@gmail.com>
2022-06-04 15:43:04 -0300

    Nuevo menú para crear Materia
    ADD: Se crea Menú para manipular la creación de una materia.

--- src/com/hjk/tp/main.c
+++ src/com/hjk/tp/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <com/hjk/tp/list/list.h>
+#include "materia/materia.h"
 
 typedef enum { ORO, ESPADA, COPA, BASTO } palo;
 
@@ -29,12 +30,47 @@ void print_carta(void *data) {
 }
 
 int main() {
+
+/*    materia* mi_materia = crear_materia("MAT2022", "Analisis I");
+
     list *lista = new_empty_list();
     carta algo = {10, COPA};
     carta otra = {1, ESPADA};
     list_add(lista, &algo, sizeof(carta));
     list_add(lista, &otra, sizeof(carta));
-    list_print(lista, &print_carta);
+    list_print(lista, &print_carta);*/
+
+    printf("Bienvenida/o al Sistema de Gestion Estudiantil SIGIL\n");
+
+    printf("Elija una de las siguientes opciones para continuar:\n\n");
+    printf("[1] Crear nueva Materia\n\n");
+
+    printf("Tu opcion: ");
+    int option_choose = -1;
+    scanf("%i", &option_choose);
+
+//    printf("%d", option_choose);
+
+    switch (option_choose) {
+        case 1:
+            printf("Indique el nombre de la materia: ");
+            char codigo_materia[10];
+            char nombre_materia[25];
+
+            scanf("%s", &nombre_materia);
+
+            printf("Ahora indique un codigo unico para identificar la materia: ");
+            scanf("%s", &codigo_materia);
+
+            materia *materia = crear_materia(codigo_materia, nombre_materia);
+
+            printf("Materia_Name: %s, Materia_Code: %s", materia->nombre, materia->id);
+
+            break;
+        default:
+            printf("Ninguna opcion configurada para ese valor.");
+    }
+
     return 0;
 }
 
