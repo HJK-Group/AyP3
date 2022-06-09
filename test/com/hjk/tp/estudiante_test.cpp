c1a985bc22f334e2e3b246b9c9d5e8a516b5da30
Kevin Eloy Tubio <kevtubio@emeal.nttdata.com>
2022-06-08 22:24:04 -0300

    revision pruebas estudiante

--- test/com/hjk/tp/estudiante_test.cpp
+++ test/com/hjk/tp/estudiante_test.cpp
@@ -5,7 +5,7 @@ extern "C" {
 }
 
 TEST(PruebasCrearEstudiante, crear_un_estudiante) {
-    estudiante *estudiante = new_estudiante(123456789, "Juan", "Perez", 20);
+    estudiante *estudiante = new_estudiante(123456789, (char *) "Juan", (char *) "Perez", 20);
     ASSERT_EQ(estudiante->legajo, 123456789);
     ASSERT_STREQ(estudiante->nombre, "Juan");
     ASSERT_STREQ(estudiante->apellido, "Perez");
@@ -15,7 +15,7 @@ TEST(PruebasCrearEstudiante, crear_un_estudiante) {
 }
 
 TEST(PruebasCrearEstudiante, estudiante_con_nombre_extenso) {
-    estudiante *estudiante = new_estudiante(123456789, "JuanManuelPerezGarciaOliverosDeLasFuentes", "Perez", 20);
+    estudiante *estudiante = new_estudiante(123456789, (char *) "JuanManuelPerezGarciaOliverosDeLasFuentes", (char *) "Perez", 20);
     ASSERT_EQ(estudiante->legajo, 123456789);
     ASSERT_STREQ(estudiante->nombre, "JuanManuelPerezGarciaOliverosDeLasFuentes");
     ASSERT_STREQ(estudiante->apellido, "Perez");
@@ -25,7 +25,7 @@ TEST(PruebasCrearEstudiante, estudiante_con_nombre_extenso) {
 }
 
 TEST(PruebasCrearEstudiante, estudiante_con_apellido_extenso) {
-    estudiante *estudiante = new_estudiante(123456789, "Juan", "PerezGarciaOliverosDeLasFuentesDeMercurio", 20);
+    estudiante *estudiante = new_estudiante(123456789, (char *) "Juan", (char *) "PerezGarciaOliverosDeLasFuentesDeMercurio", 20);
     ASSERT_EQ(estudiante->legajo, 123456789);
     ASSERT_STREQ(estudiante->nombre, "Juan");
     ASSERT_STREQ(estudiante->apellido, "PerezGarciaOliverosDeLasFuentesDeMercurio");
@@ -35,7 +35,7 @@ TEST(PruebasCrearEstudiante, estudiante_con_apellido_extenso) {
 }
 
 TEST(PruebasCrearEstudiante, nombre_con_espacios) {
-    estudiante *estudiante = new_estudiante(123456789, "Juan Marcos", "Perez", 20);
+    estudiante *estudiante = new_estudiante(123456789, (char *) "Juan Marcos", (char *) "Perez", 20);
     ASSERT_EQ(estudiante->legajo, 123456789);
     ASSERT_STREQ(estudiante->nombre, "Juan Marcos");
     ASSERT_STREQ(estudiante->apellido, "Perez");
@@ -45,7 +45,7 @@ TEST(PruebasCrearEstudiante, nombre_con_espacios) {
 }
 
 TEST(PruebasCrearEstudiante, apellido_con_espacios) {
-    estudiante *estudiante = new_estudiante(123456789, "Juan", "Perez Garcia", 20);
+    estudiante *estudiante = new_estudiante(123456789, (char *) "Juan", (char *) "Perez Garcia", 20);
     ASSERT_EQ(estudiante->legajo, 123456789);
     ASSERT_STREQ(estudiante->nombre, "Juan");
     ASSERT_STREQ(estudiante->apellido, "Perez Garcia");
