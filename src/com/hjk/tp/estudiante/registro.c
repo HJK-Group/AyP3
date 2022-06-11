aedf5054a34d6c0886bd83ff0a1c3f661051e0fc
Juan <juan.m.calvino@gmail.com>
2022-06-11 13:11:37 -0300

    add registro_buscar_por_nombre

--- src/com/hjk/tp/estudiante/registro.c
+++ src/com/hjk/tp/estudiante/registro.c
@@ -2,17 +2,48 @@
 #include "registro.h"
 #include "string.h"
 
+int comparar_edad(void *primer_estudiante, void *segundo_estudiante);
+int comparar_nombre(void *primer_estudiante, void *segundo_estudiante);
+
 int buscar_nombre(void *data, void *other_data) {
     estudiante *alumno = (estudiante *) data;
     char *nombre = (char *) other_data;
     return strcmp(alumno->nombre, nombre);
 }
 
+
+listado *registro_buscar_por_edad(registro *listado_alumnos, short desde, short hasta) {
+
+    if (listado_alumnos == NULL || listado_alumnos->listado_por_edad->generic_list->length == 0) {
+        return NULL;
+    }
+    if (desde > hasta || desde < 0 || hasta > listado_alumnos->listado_por_edad->generic_list->length) {
+        return NULL;
+    }
+
+    listado *listado_result = new_empty_ordered_list(&comparar_edad);
+    ordered_list *listado_ordenado_edad = listado_alumnos->listado_por_edad;
+    node *next_estudiante = listado_ordenado_edad->generic_list->head;
+
+
+    estudiante *estudiante_actual = (estudiante *)next_estudiante->data;
+
+    while (next_estudiante != NULL && estudiante_actual != NULL && estudiante_actual->edad <= hasta) {
+        if (estudiante_actual->edad >= desde) {
+            ordered_list_add(listado_result, estudiante_actual);
+        }
+            next_estudiante = next_estudiante->next;
+            estudiante_actual = (estudiante *) next_estudiante->data;
+    }
+    return  listado_result;
+}
+
 estudiante *registro_buscar_por_nombre(registro *listado_alumnos, char *nombre) {
     if (listado_alumnos == NULL || nombre == NULL)
         return NULL;
 
-    return (estudiante *) ordered_list_search_data(listado_alumnos->listado_por_nombre, &buscar_nombre, (void *) nombre);
+    return (estudiante *) ordered_list_search_data(listado_alumnos->listado_por_nombre, &buscar_nombre,
+                                                   (void *) nombre);
 }
 
 int comparar_nombre(void *primer_estudiante, void *segundo_estudiante) {
@@ -35,24 +66,17 @@ void registro_agregar_alumno(registro *listado_alumnos, estudiante *alumno) {
     ordered_list_add((ordered_list *) listado_alumnos->listado_por_edad, alumno);
 }
 
-listado *registro_buscar_por_edad(registro *listado_alumnos, short desde, short hasta) {
-    if (listado_alumnos == NULL)
-        return NULL;
-
-    return listado_alumnos->listado_por_edad;
-} //TODO TERMINAR!
-
 int registro_remover_estudiante(registro *listado_alumnos, estudiante *alumno) {
-    return ordered_list_remove_data((ordered_list*) listado_alumnos->listado_por_nombre, alumno, sizeof(estudiante));
+    return ordered_list_remove_data((ordered_list *) listado_alumnos->listado_por_nombre, alumno, sizeof(estudiante));
 }
 
 void listar_registro(registro *listado_alumnos) {
-    ordered_list* listado_por_nombre = listado_alumnos->listado_por_nombre;
+    ordered_list *listado_por_nombre = listado_alumnos->listado_por_nombre;
     ordered_list_print(listado_por_nombre, function_print_registro);
 }
 
-void function_print_registro(void* data) {
-    estudiante* un_estudiante = data;
+void function_print_registro(void *data) {
+    estudiante *un_estudiante = data;
 
     printf("%s\n", un_estudiante->nombre);
     printf("%s\n", un_estudiante->apellido);
